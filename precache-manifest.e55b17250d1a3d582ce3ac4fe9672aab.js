9818c6d99b2ebb0ecdc376a993edc89398b67f61
Antoine Julienne <antoine.julienne@gmail.com>
2020-03-02 00:19:11 +0100

    Updates

---
+++ precache-manifest.e55b17250d1a3d582ce3ac4fe9672aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bba0782335165418c559f0bf2161104",
+    "url": "/react-game-of-life/index.html"
+  },
+  {
+    "revision": "663d801a3ac9e3cf76e3",
+    "url": "/react-game-of-life/static/css/main.a5a71c78.chunk.css"
+  },
+  {
+    "revision": "d4d8858cb31bb1f6e1a0",
+    "url": "/react-game-of-life/static/js/2.239beab8.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/react-game-of-life/static/js/2.239beab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663d801a3ac9e3cf76e3",
+    "url": "/react-game-of-life/static/js/main.d1ac01df.chunk.js"
+  },
+  {
+    "revision": "bbaa4330c6218ff63ecc",
+    "url": "/react-game-of-life/static/js/runtime-main.f0415db4.js"
+  }
+]);
